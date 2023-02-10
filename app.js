e8b998a22f1ab185b1b3bc5daa9d6fdd108b5509
Abul <108424831+AbulfaslAhmadi@users.noreply.github.com>
2023-02-10 17:49:36 +0100

    v.1.1.1

--- app.js
+++ app.js
@@ -1,28 +1,48 @@
 document.addEventListener("scroll", () => {
-    let scroll = window.scrollY
-    leftScroll = scroll / -1.4
-    rightScroll = scroll / 1.5
+    const scroll = window.scrollY;
 
-    let SecOneLeft = document.querySelectorAll(".sec-one-left");
-    let SecOneRight = document.querySelectorAll(".sec-one-right");
+
+    /* hero title */
+    const titleScroll = scroll / 1.4;
+
+    const SecOneLeft = document.querySelectorAll(".sec-one-left");
+    const SecOneRight = document.querySelectorAll(".sec-one-right");
 
     for(i=0; i<SecOneRight.length; i++) {
        if(scroll < 500) {
-        SecOneRight[i].style.transform = `translate(${rightScroll}px)`
-        SecOneLeft[i].style.transform = `translate(${leftScroll}px)`
+        SecOneRight[i].style.transform = `translate(${titleScroll}px)`;
+        SecOneLeft[i].style.transform = `translate(${-titleScroll}px)`;
        }
     }
 
-    let upBTN = document.querySelector(".go-up-btn");
+
+    /* go up btn */
+
+    const upBTN = document.querySelector(".go-up-btn");
 
     if(scroll > 400) {
-        upBTN.classList.add("go-up-btn-active")
+        upBTN.classList.add("go-up-btn-active");
     } else {
-        upBTN.classList.remove("go-up-btn-active")
+        upBTN.classList.remove("go-up-btn-active");
+    }
+
+
+    /* comments on sec 3 */
+
+    const commentOne = document.querySelector(".sec-three-comment-one");
+    const commentTwo = document.querySelector(".sec-three-comment-two");
+
+    const commentsScroll = (scroll - 1200) / 1.4;
+    console.log(commentsScroll)
+
+    if(commentsScroll > -300 && commentsScroll < 400) {
+        commentOne.style.transform = `translate(${commentsScroll}px) rotate(${-20 + (commentsScroll / 4)}deg)`;
+        commentTwo.style.transform = `translate(${-commentsScroll}px) rotate(${10 + (commentsScroll / 4)}deg)`;
     }
 })
 
 
+/* waiting to be called by goUpBTN */
 function goUp() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
