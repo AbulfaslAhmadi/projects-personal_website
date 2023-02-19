88f90077caeba6ce3339b2d4c9d11201e006b52f
Abul <108424831+AbulfaslAhmadi@users.noreply.github.com>
2023-02-19 19:04:44 +0100

    v.2.3.2

--- mobile/app.js
+++ mobile/app.js
@@ -63,3 +63,11 @@ function goUp() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+
+/* checking width and redirecting */
+window.addEventListener("resize", () => {
+    if(window.screen.width > 750) {
+        document.location = "/index.html";
+    }
+})
